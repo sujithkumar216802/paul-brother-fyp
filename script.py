4e041758effb7e699319857fffedfd3774f92fdf
S Sujith Kumar <sujithkumar216802@gmail.com>
2023-05-14 00:04:03 +0530

    idk

--- script.py
+++ script.py
@@ -2,11 +2,12 @@ import os
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.model_selection import train_test_split
+from sklearn.metrics import explained_variance_score, mean_squared_error
 from sklearn.pipeline import make_pipeline
 from sklearn.decomposition import PCA
 from sklearn.cross_decomposition import PLSRegression
 from sklearn.linear_model import LinearRegression, Lasso, Ridge
+from sklearn.model_selection import cross_val_predict
 
 wavelength = 'Wavelength(nm)'
 intensity = 'Intensity(a.u.)'
@@ -17,8 +18,10 @@ def read_and_preprocess(data_dir='forehead data'):
     target_df = pd.DataFrame(columns=['name', 'target'])
     names_of_files = os.listdir(data_dir)
     for filename in names_of_files:
-        raw_values = pd.read_csv(data_dir + '\\' + filename, delim_whitespace=True, header=0)
-        raw_values_400_to_900_df = raw_values[raw_values[wavelength].between(400, 900)]
+        raw_values = pd.read_csv(
+            data_dir + '\\' + filename, delim_whitespace=True, header=0)
+        raw_values_400_to_900_df = raw_values[raw_values[wavelength].between(
+            400, 900)]
         values_400_to_900_df[wavelength] = raw_values_400_to_900_df[wavelength]
 
         # with np.errstate(divide='raise', invalid='raise'):
@@ -27,7 +30,7 @@ def read_and_preprocess(data_dir='forehead data'):
         #     except FloatingPointError:
         #         print(f'Invalid values in {filename}')
         #         continue
-        
+
         # try:
         #     all_target_df[all_target_df['name'] == filename]['target'].to_numpy()[0]
         # except IndexError:
@@ -38,19 +41,23 @@ def read_and_preprocess(data_dir='forehead data'):
             "name": filename,
             "target": all_target_df[all_target_df['name'] == filename]['target'].to_numpy()[0]
         }
-        
+
         # SNV
-        log_intensity_400_to_900 = -np.log10(raw_values_400_to_900_df[intensity].to_numpy())
+        log_intensity_400_to_900 = - \
+            np.log10(raw_values_400_to_900_df[intensity].to_numpy())
         mean = np.mean(log_intensity_400_to_900)
         std = np.std(log_intensity_400_to_900)
-        values_400_to_900_df[filename] = (mean - log_intensity_400_to_900) / std
+        values_400_to_900_df[filename] = (
+            mean - log_intensity_400_to_900) / std
 
         plt.figure(0)
         plt.plot(wavelength, intensity, data=raw_values, label=filename)
         plt.figure(1)
-        plt.plot(wavelength, intensity, data=raw_values_400_to_900_df, label=filename)
+        plt.plot(wavelength, intensity,
+                 data=raw_values_400_to_900_df, label=filename)
         plt.figure(2)
-        plt.plot(values_400_to_900_df[wavelength], values_400_to_900_df[filename], label=filename)
+        plt.plot(values_400_to_900_df[wavelength],
+                 values_400_to_900_df[filename], label=filename)
 
     plt.figure(0)
     plt.title('All')
@@ -71,9 +78,8 @@ def read_and_preprocess(data_dir='forehead data'):
     values_400_to_900_df = values_400_to_900_df.set_index(wavelength)
     return values_400_to_900_df.T, target_df
 
-values_df, target_df = read_and_preprocess()
-target = target_df['target'].to_numpy()
-x_train, x_test, y_train, y_test = train_test_split(values_df, target, random_state=42)
+x_train, target_df = read_and_preprocess(data_dir='ffs')
+y_train = target_df['target'].to_numpy()
 
 
 # PCR related stuff
@@ -82,86 +88,72 @@ for i in range(1, x_train.shape[0] + 1):
     pca = PCA(n_components=i)
     pca.fit(x_train)
     pca_explained_variance.append(pca.explained_variance_ratio_.sum())
-
+    
 plt.figure(3)
-plt.plot(range(1, x_train.shape[0] + 1), pca_explained_variance, '-ro')
+plt.plot(range(1, x_train.shape[0] + 1), pca_explained_variance, 'r-o', label='PCR', markerfacecolor='none')
 
-# # PLS related stuff
-# pls_explained_variance = []
-# for i in range(1, values.shape[0] + 1):
-#     pls = PLSRegression(n_components=i)
-#     pls.fit(values, target)    
-
-#     variance_in_x = np.var(pls.x_scores_, axis = 0) 
-#     fractions_of_explained_variance = variance_in_x / np.sum(variance_in_x)
-#     pls_explained_variance.append(fractions_of_explained_variance)
-
-# plt.figure(4)
-# plt.plot(range(1, values.shape[0] + 1), pls_explained_variance, '-ro')
-
-
+# PLS related stuff
+pls_explained_variance = []
 for i in range(1, x_train.shape[0] + 1):
-    pcr = make_pipeline(PCA(n_components=i), LinearRegression())
-    pcr.fit(x_train, y_train)
-    plt.figure(4)
-    plt.scatter(y_test, pcr.predict(x_test), label=f'{i} components')
-
-    pcr = make_pipeline(PCA(n_components=i), Lasso())
-    pcr.fit(x_train, y_train)
-    plt.figure(5)
-    plt.scatter(y_test, pcr.predict(x_test), label=f'{i} components')
-
-    pcr = make_pipeline(PCA(n_components=i), Ridge())
-    pcr.fit(x_train, y_train)
-    plt.figure(6)
-    plt.scatter(y_test, pcr.predict(x_test), label=f'{i} components')
-
     pls = PLSRegression(n_components=i)
     pls.fit(x_train, y_train)
-    plt.figure(7)
-    plt.scatter(y_test, pls.predict(x_test), label=f'{i} components')
+    pls_explained_variance.append(explained_variance_score(y_train, pls.predict(x_train)))
 
-plt.figure(4)
-plt.xlabel('True values')
-plt.ylabel('Predicted values')
-plt.title('PCR using linear')
-plt.legend(loc="upper right")
+plt.figure(3)
+plt.plot(range(1, x_train.shape[0] + 1), pls_explained_variance, 'b-^', label='PLS', markerfacecolor='none')
+plt.xlabel('Number of Principal Components')
+plt.ylabel('Explained variance')
+plt.legend(loc="lower right")
+
+
+comp = 8
+
+pcr = make_pipeline(PCA(n_components=comp), LinearRegression())
+pcr.fit(x_train, y_train)
 plt.figure(5)
-plt.xlabel('True values')
-plt.ylabel('Predicted values')
-plt.title('PCR using lasso')
-plt.legend(loc="upper right")
-plt.figure(6)
-plt.xlabel('True values')
-plt.ylabel('Predicted values')
-plt.title('PCR using ridge')
-plt.legend(loc="upper right")
-plt.figure(7)    
-plt.xlabel('True values')
-plt.ylabel('Predicted values')
-plt.title('PLS')
-plt.legend(loc="upper right")
+plt.scatter(y_train, pcr.predict(x_train), facecolor='none', color='red',  label=f'PCR with {comp} components')
 
-linear = LinearRegression()
-linear.fit(x_train, y_train)
+pls = PLSRegression(n_components=comp)
+pls.fit(x_train, y_train)
+plt.figure(5)
+plt.scatter(y_train, pls.predict(x_train), facecolor='none', color='blue', label=f'PLSR with {comp} components')
+
+plt.figure(5)
+plt.xlabel('Observed Response')
+plt.ylabel('Fitted Response')
+plt.legend(loc="upper left")
+
+comp_in_loadings = 1 # 0-indexing.. 0 is 1, 1 is 2, etc
+plt.figure(7)
+plt.plot(x_train.columns, (pls.x_loadings_.T[comp_in_loadings]).T, label=f'PLSR Component no.{comp_in_loadings + 1}')
+plt.xlabel(wavelength)
+plt.ylabel('PLSR Loadings')
+plt.legend(loc="upper left")
+
 plt.figure(8)
-plt.scatter(y_test, linear.predict(x_test))
-plt.xlabel('True values')
-plt.ylabel('Predicted values')
-plt.title('linear regression')
+plt.plot(x_train.columns, pca.components_[comp_in_loadings].T, label=f'PCR Component no. {comp_in_loadings + 1}')
+plt.xlabel(wavelength)
+plt.ylabel('PCA Loadings')
+plt.legend(loc="upper left")
 
-lasso = Lasso()
-lasso.fit(x_train, y_train)
-plt.figure(9)
-plt.scatter(y_test, lasso.predict(x_test))
-plt.xlabel('True values')
-plt.ylabel('Predicted values')
-plt.title('lasso regression')
+pls_mspe_array = []
+pcr_mspe_array = []
+for i in range(1, x_train.shape[0] + 1):
+    pls = PLSRegression(n_components=i)
+    pls.fit(x_train, y_train)
+    pls_pred = pls.predict(x_train)
+    pls_mspe = mean_squared_error(y_train, pls_pred)
+    pls_mspe_array.append(pls_mspe)
 
-ridge = Ridge()
-ridge.fit(x_train, y_train)
-plt.figure(10)
-plt.scatter(y_test, ridge.predict(x_test))
-plt.xlabel('True values')
-plt.ylabel('Predicted values')
-plt.title('ridge regression')
+    pcr = make_pipeline(PCA(n_components=i), LinearRegression())
+    pcr.fit(x_train, y_train)
+    pcr_pred = pcr.predict(x_train)
+    pcr_mspe = mean_squared_error(y_train, pcr_pred)
+    pcr_mspe_array.append(pcr_mspe)
+
+plt.figure(6)
+plt.plot(range(1, x_train.shape[0] + 1), pcr_mspe_array, 'r-o', label='PCR', markerfacecolor='none')
+plt.plot(range(1, x_train.shape[0] + 1), pls_mspe_array, 'b-^', label='PLS', markerfacecolor='none')
+plt.xlabel('Number Of Components')
+plt.ylabel('Estimated Mean Squared Prediction Error')
+plt.legend(loc="lower right")
